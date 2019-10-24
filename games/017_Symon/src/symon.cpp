c454dbf9708500567ee4321e87f61df553634532
Leo Trottier <trottier@gmail.com>
2019-10-24 16:21:57 -0700

    tmm 7 rr 10

--- games/017_Symon/src/symon.cpp
+++ games/017_Symon/src/symon.cpp
@@ -74,8 +74,8 @@ int currentLevel = 14; // LEVELS START AT 10
 const int MIN_LEVEL =      13;   // Number of past interactions to look at for performance
 const int HISTORY_LENGTH=      7;   // Number of past interactions to look at for performance
 const int ENOUGH_SUCCESSES=    6;   // if successes >= ENOUGH_SUCCESSES level-up
-const int TOO_MANY_MISSES=     6;   // if num misses >= TOO_MANY_MISSES level-down
-const int REINFORCE_RATIO =      30; // the foodtreat reinforcement ratio [0-100] 100:always foodtreat
+const int TOO_MANY_MISSES=     7;   // if num misses >= TOO_MANY_MISSES level-down
+const int REINFORCE_RATIO =      10; // the foodtreat reinforcement ratio [0-100] 100:always foodtreat
 // LED colors and intensities
 const int CUE_LIGHT_PRESENT_INTENSITY_RED = 99; // [0-99] // cue / status light is yellow in present phase
 const int CUE_LIGHT_PRESENT_INTENSITY_GREEN = 99; // [0-99]
